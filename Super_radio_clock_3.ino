866e513ff93cbaf63aefb5f83bb58868039552ff
kodera2t <kodera2t@yahoo.co.jp>
2018-07-02 20:00:04 +0900

    introduction is changed.

--- Super_radio_clock_3.ino
+++ Super_radio_clock_3.ino
@@ -1,19 +1,7 @@
 /*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-This example is for a 128x64 size display using I2C to communicate
-3 pins are required to interface (2 I2C and one reset)
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
+Firmware program for my new radio system, available at
+https://www.tindie.com/products/microwavemont/dsp-radio-chip-based-lw-mw-sw-fm-receiver/
+This program is released "as is" and without any warranty.
 *********************************************************************/
 
 #include <SPI.h>
